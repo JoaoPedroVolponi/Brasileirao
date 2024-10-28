c9e94d5b8cea24aea1740e2bc44b4e72b0748c3f
João Pedro Volponi <jpvrosada@hotmail.com>
2024-10-27 23:24:35 -0300

    HomeView e HomeViewModel

---
+++ Brasileirao/Home/HomeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewModel.swift
+//  Brasileirao
+//
+//  Created by João Pedro Volponi on 27/10/24.
+//
+
+import Foundation
+import UIKit
+
+protocol HomeViewModelProtocol {
+    
+}
+
+class HomeViewModel: HomeViewModelProtocol {
+    
+}
