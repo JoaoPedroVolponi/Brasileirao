bc1624d4d426235934637c93dbb8d67653ae6558
João Pedro Volponi <jpvrosada@hotmail.com>
2024-10-27 23:12:07 -0300

    Criando Projeto

---
+++ Brasileirao/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Brasileirao
+//
+//  Created by João Pedro Volponi on 27/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
